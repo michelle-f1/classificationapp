20c4616f7ae27fc9e89d0befa35edd471dd3ce27
Michelle <michelle.f494@gmail.com>
2020-10-29 20:57:31 +0200

    frontend

---
+++ client/src/components/Navbar.vue
@@ -0,0 +1,51 @@
+<template>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <router-link class="navbar-brand" to="/">Data Classification</router-link>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+
+  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <ul class="navbar-nav ml-auto">
+      <li class="nav-item active">
+        <router-link to="/" class="nav-link">
+            Home 
+            <span class="sr-only">(current)</span>
+        </router-link>
+      </li>
+      <li class="nav-item">
+        <router-link to="/about" class="nav-link">
+            About
+        </router-link>
+        </li>
+        <!--Login-->
+        <li class="nav-item">
+        <router-link to="/login" class="nav-link">Login</router-link>
+        </li>
+         <!--Register-->
+        <li class="nav-item">
+        <router-link to="/register" class="nav-link">Register</router-link>
+      </li>
+        <!--Profile-->
+        <li class="nav-item">
+        <router-link to="/profile" class="nav-link">Profile</router-link>
+      </li>
+      <!--Logout-->
+        <li class="nav-item">
+        <a to="/logout" class="nav-link">Logout</a>
+      </li>
+    </ul>
+    
+  </div>
+</nav>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style scoped>
+
+</style>
