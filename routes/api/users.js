c3d71ce2b13401c7f3ae7ef6e952d1922f7076f9
Michelle <michelle.f494@gmail.com>
2020-10-29 10:31:15 +0200

    user data

---
+++ routes/api/users.js
@@ -0,0 +1,126 @@
+const express = require('express');
+const router = express.Router();
+//hash passwords
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const key = require('../../config/keys').secret;
+const passport = require('passport'); //used to predict routes
+const User = require('../../model/User');
+/**
+ * @route POST api/users/register
+ * @desc Register the user
+ * @access Public
+ */
+router.post('/register', (req, res) => {
+    let{
+        name,
+        username,
+        email,
+        password,
+        confirm_password
+    } = req.body
+    if(password !== confirm_password){
+        return res.status(400).json({
+            msg: "Password and confirm-password do not match" //password match check sending response if no match
+        });
+    }
+    //validate username
+    User.findOne({
+        username: username
+    }).then(user => {
+        if(user) {
+            return res.status(400).json({
+                msg: "This username is already in use."
+            });
+        }
+    })
+    //validate email
+    User.findOne({
+        email: email
+    }).then(user => {
+        if(user) {
+            return res.status(400).json({
+                msg: "This email has already been registered."
+            });
+        }
+    });
+    //If nothing goes wrong then the new user can be registered
+    let newUser = new User({
+        name,
+        username,
+        password,
+        email
+    });
+    //hash password
+    bcrypt.genSalt(10, (err, salt) => {
+        bcrypt.hash(newUser.password, salt, (err, hash) => {
+            if(err) throw err;
+            newUser.password = hash;
+            newUser.save().then(user => {
+                return res.status(201).json({
+                    success:true,
+                    msg: "User has been registered."
+                });
+            });
+        });
+
+    });
+});
+
+/**
+ * @route POST api/users/login
+ * @desc Signing in the user
+ * @access Public
+ */
+router.post('/login', (req, res) => {
+    User.findOne({ username: req.body.username}).then(user => {
+        if(!user) {
+            return res.status(404).json({
+                msg: "Username entered not found",
+                success: false
+            });
+        }
+        //user found now validate password
+        bcrypt.compare(req.body.password, user.password).then(isMatch => {
+            if(isMatch) {
+                //JSONtoken should be sent to user
+                const payload ={
+                    _id: user._id,
+                    username: user.username,
+                    name: user.name,
+                    email: user.email
+                }
+                //using jwt package
+                jwt.sign(payload, key, { 
+                    expiresIn: 604800    //token expiretion
+                }, (err, token) => {
+                    res.status(200).json({
+                        success: true,
+                        token: 'Bearer '+token,
+                        msg: "Login Successful!"   //message to indicate successful login
+                    });
+                })
+            } else {
+                return res.status(404).json({
+                    msg: "Incorrect password entered.",
+                    success: false
+                });
+            }
+        })
+    });
+});
+
+/**
+ * @route POST api/users/profile
+ * @desc Return user data
+ * @access Private
+ */
+router.get('/profile', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    return res.json({
+        user: req.user
+    });
+});
+
+module.exports = router;
